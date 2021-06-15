be023b7f67c752088f65f7739ad557f2515231e3
pichayakorn <pratch12652@gmail.com>
2021-06-15 23:42:30 +0700

    Fix indent

--- index.js
+++ index.js
@@ -11,31 +11,31 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-  // set a new item in the Collection
-  // with the key as the command name and the value as the exported module
-  client.commands.set(command.name, command);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
 }
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('MEAOW is ready!');
+    console.log('MEAOW is ready!');
 });
 
 client.on('message', function(message) {
 
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
+    if (!client.commands.has(command)) return;
 
-	try {
-		client.commands.get(command).execute(message, args);
+    try {
+        client.commands.get(command).execute(message, args);
 	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
 	}
 
 });
